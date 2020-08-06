6cf3e88eefd1b8dc9af0cfc4f1900f61c86ef950
kdj <kdj@hansero.co.kr>
2020-08-06 11:08:22 +0900

    test

--- H_Czech_Under_Body_Image_Acquisition_W/MainEngine.cs
+++ H_Czech_Under_Body_Image_Acquisition_W/MainEngine.cs
@@ -208,6 +208,7 @@ namespace H_Czech_Under_Body_Image_Acquisition_W
             {
                 try
                 {
+                    Console.Write("");
                     return Math.Round((1000.0 / FrameRate));
                 }
                 catch
